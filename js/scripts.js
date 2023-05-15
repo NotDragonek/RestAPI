5ba6216394eb1fa3ccfb245e1a73667088386e5b
Your Name <you@example.com>
2023-05-15 13:58:02 +0200

    refactor

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,32 @@
 'use strict'
 
-const btn = document.querySelector("button")
+const btn = document.querySelector("button");
 
-const getData = () => {
+const baseUrl = 'https://api.nbp.pl/api/exchangerates/tables/'
+const getCurrency = async (table) => {
+                    
+                    try {
+                    const response = await fetch(`${baseUrl}${table}`);
+                    const data = await response.json();
+                    return data;
+                    } catch(err) {
+                        console.log(`to jest błąd: ${err}`)
+                    }
+                    
+                    
+                    }
 
-    fetch('https://api.nbp.pl/api/exchangerates/tables/A/?format=json')
-    .then(response => response.json())
-    .then(result => {
-        console.log(result);
+getCurrency('a').then((data) => {
+    console.log(data);
+    // const currencies = data[0]
+    //destrukturyzacja 
+    const [currencies] = data
 
-        result[0].rates.forEach((elem, i) => {
-            const tr = document.createElement('tr')
+    const {rates} = currencies
+
+    rates.forEach((elem , i) => {
+        
+        const tr = document.createElement('tr')
             tr.innerHTML = 
             `
             <td>${++i}</td>
@@ -21,10 +37,32 @@ const getData = () => {
 
             document.querySelector('table tbody').appendChild(tr);
 
-        })
-
     });
+});
 
-}
+// const getData = () => {
 
-btn.addEventListener('click', getData)
+//     fetch(baseUrl)
+//     .then(response => response.json())
+//     .then(result => {
+//         console.log(result);
+
+//         result[0].rates.forEach((elem, i) => {
+//             const tr = document.createElement('tr')
+//             tr.innerHTML = 
+//             `
+//             <td>${++i}</td>
+//             <td>${elem.currency}</td>
+//             <td>${elem.mid}</td>
+            
+//             `
+
+//             document.querySelector('table tbody').appendChild(tr);
+
+//         })
+
+//     });
+
+// }
+
+// btn.addEventListener('click', getData)
